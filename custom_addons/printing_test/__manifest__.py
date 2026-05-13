fbeeff2829dce748b46911550575891792429480
YourName <your-email@example.com>
2026-05-13 16:47:43 +0530

    Added priority field and manifest configuration

---
+++ custom_addons/printing_test/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'Printing and Contact Extension',
+    'version': '1.0',
+    'category': 'Tools',
+    'summary': 'Learning inheritance and printing',
+    'depends': ['base', 'contacts'],
+    'data': ['views/res_partner_views.xml'],
+    'installable': True,
+    'application': True,
+}
